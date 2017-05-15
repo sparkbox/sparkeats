11dc18e95cdf1958f0f6559d7b89e91f3f0067a5
Betty Baker <betty@heysparkbox.com>
2017-05-15 14:50:29 -0400

    Add comments to hello-world-handlebars.js

--- source/js/hello-world-handlebars.js
+++ source/js/hello-world-handlebars.js
@@ -1,4 +1,11 @@
+// Define the template
 var template = 'Hello, {{foo}}!';
+
+// Compile the template
 var render = Handlebars.compile(template);
+
+// Define the data
 var data = {foo: 'world'};
+
+// Rendering the template
 console.log(render(data));
