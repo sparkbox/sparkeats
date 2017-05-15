c448766124d55ee663c69883c75aa6c5f583256f
Betty Baker <betty@heysparkbox.com>
2017-05-15 15:59:57 -0400

    :racehorse: Build data parser and hbs compiler

--- source/js/hello-world-handlebars.js
+++ source/js/hello-world-handlebars.js
@@ -1,14 +1,23 @@
 // Require Handlebars
 var Handlebars = require('handlebars');
 
+// Require FS
+var fs = require('fs');
+
+// Require yamljs
+var YAML = require('yamljs');
+
 // Define the template
-var template = 'Hello, {{foo}}!';
+var template = fs.readFileSync('source/templates/layouts/default.hbs');
 
 // Compile the template
-var render = Handlebars.compile(template);
+var render = Handlebars.compile(template.toString());
 
 // Define the data
-var data = {foo: 'world'};
+var yml = fs.readFileSync('source/data/data.yml');
+
+// Parse the data to a json file
+var data = YAML.parse(yml.toString());
 
 // Rendering the template
 console.log(render(data));
