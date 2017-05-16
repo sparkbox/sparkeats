2904540e8faf4e06cf27025f79e06beee99f7e54
Heather Taylor <heather@heysparkbox.com>
2017-05-16 16:37:51 -0400

    feat: create postcss configuration file
    - created postcss.config.json file to set a list of browser autoprefixers
    - test postcss.config.json file by manipulating the main.scss file
    - found the "postsass" script in the package.json file was causing the "sass" script to loop continously. I don't understand why, but once I changed the name of the script from "postsass" to "styles", it worked fine.

--- tasks/postcss.js
+++ tasks/postcss.js
@@ -3,4 +3,4 @@
 const shell = require('shelljs');
 
 // use postcss to parse CSS and add vendor prefixes to CSS rules
-shell.exec('postcss -u autoprefixer -r public/assets/css/main.css');
+shell.exec('postcss --use autoprefixer -c ./tasks/postcss.config.json public/assets/css/main.css -d public/assets/css');
