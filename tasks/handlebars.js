734258268c27f5e3c1dca7849b0c8742b849b09e
Betty Baker <betty@heysparkbox.com>
2017-05-22 10:51:04 -0400

    Update watch task

--- tasks/handlebars.js
+++ tasks/handlebars.js
@@ -14,14 +14,14 @@ const templates = globby.sync('source/pages/*.hbs');
 // Loop through the array of templates
 templates.forEach( function(template) {
 
-    // Define the content (html)
+    // WHAT IS `template` that requires you to call `toString`?
+    console.log('Type of template: ', typeof template );
+    console.log('Value of template: ', template);
+
+// =================== PREP TEMPLATE =============================
+
+    // Define the content
     const html = fs.readFileSync(template.toString());
-
-    // Define the data
-    const yml = fs.readFileSync('source/data/data.yml');
-
-    // Parse the data to a json file
-    const data = YAML.parse(yml.toString());
 
     // Compile the template
     const compileTemplate = Handlebars.compile(html.toString());
@@ -33,6 +33,16 @@ templates.forEach( function(template) {
     // Currently only pulls from pages/
     // TODO include partials and layouts
     const newFilePath = srxp(newFileExtension).match('source/pages/').replace('public/').text();
+
+// =================== PREP DATA =============================
+
+    // Define the data
+    const yml = fs.readFileSync('source/data/data.yml');
+
+    // Parse the data to JSON
+    const data = YAML.parse(yml.toString());
+
+// =================== COMBINE TEMPLATE & DATA =============================
 
     // Builds the html file using the template and data
     const writeFile = fs.writeFileSync(newFilePath, compileTemplate(data));
--- tasks/watch-hbs.js
+++ tasks/watch-hbs.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const shell = require('shelljs');
-const onchange = require('onchange');
 
 // using onchange - throws an exec: internal error upon quitting
 shell.exec("onchange 'source/pages/*.hbs' -- npm run html");
