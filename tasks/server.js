5a231632dc0a31f8f8783924e2df26cfca15f892
Heather Taylor <heather@heysparkbox.com>
2017-05-22 16:41:13 -0400

    feat: add required modules
    - require express and path
    - set app variable to use express module

--- tasks/server.js
+++ tasks/server.js
@@ -1,10 +1,14 @@
 'use strict';
 
 const shell = require('shelljs');
-const browser = require('browser-sync');
+//const browser = require('browser-sync');  -- don't need
+const express = require('express');
+const app = express();
+const path = require('path');
 
 if (process.env.NODE_ENV === 'production'){
   // start production server
+
 }
 else {
   // start development server to watch and update html and css files
