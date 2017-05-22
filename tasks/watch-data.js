acb894390c6587f9f77624c306cd8feb8a637993
Betty Baker <betty@heysparkbox.com>
2017-05-22 11:33:41 -0400

    Consolidate watch scripts into watch.js

--- tasks/watch-data.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const shell = require('shelljs');
-const onchange = require('onchange');
-
-// using onchange - throws an exec: internal error upon quitting
-shell.exec("onchange 'source/data/data.yml' -- npm run html");
