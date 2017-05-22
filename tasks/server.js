ea528140e26a19b834472ac36924f414e079936d
Heather Taylor <heather@heysparkbox.com>
2017-05-22 16:43:45 -0400

    test: add console.log statements
    console.log statements to help in testing of production and development servers.

--- tasks/server.js
+++ tasks/server.js
@@ -8,9 +8,11 @@ const path = require('path');
 
 if (process.env.NODE_ENV === 'production'){
   // start production server
+  console.log("You have accessed the production server.");
 
 }
 else {
   // start development server to watch and update html and css files
+  console.log("You have accessed the development server.");
   shell.exec("browser-sync start --server --files 'public/assets/css/*.css, *.html'");
 }
