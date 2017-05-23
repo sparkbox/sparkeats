73623f92965aac26ec0fdecd1a28e9b7d3de6253
Betty Baker <betty@heysparkbox.com>
2017-05-23 13:40:30 -0400

    :racehorse: Refactor: Use node path instead of regex

--- tasks/handlebars.js
+++ tasks/handlebars.js
@@ -1,50 +1,22 @@
 const globby = require('globby');
 const fs = require('fs');
+const path = require('path');
 const Handlebars = require('handlebars');
-const srxp = require('simple-regexp');
 const YAML = require('yamljs');
 
+// This script compiles Handlebar templates, data
+// and writes a new HTML file in the public/ directory
+// New files are created with the same name as the Handlebar template.
+// Existing files are replaced.
 
+const data = YAML.load('source/data/data.yml');
+const files = globby.sync('source/pages/*.hbs');
 
-// Define the templates
-  // Creates an array of file paths (strings)
-  // pulls from the pages directory
-  // TODO include partials and layouts
-const templates = globby.sync('source/pages/*.hbs');
+files.forEach( function(file) {
+    const html = fs.readFileSync(file).toString();
+    const template = Handlebars.compile(html);
+    const fileName = path.basename(file, '.hbs');
+    const newFilePath = `public/${fileName}.html`;
 
-// Loop through the array of templates
-templates.forEach( function(template) {
-
-// =================== PREP TEMPLATE =============================
-
-    // Define the content
-    // fs.readFileSync returns an object (buffer)
-    const html = fs.readFileSync(template);
-
-    // Compile the template
-    // Handlebars.compile requires a string and will not take an object
-    const compileTemplate = Handlebars.compile(html.toString());
-
-    // Change file extension from .hbs to .html
-    const newFileExtension = srxp(template).match('.hbs').replace('.html').text();
-
-    // Change file path to html build directory
-    // Currently only pulls from pages/
-    // TODO include partials and layouts
-    const newFilePath = srxp(newFileExtension).match('source/pages/').replace('public/').text();
-
-// =================== PREP DATA =============================
-
-    // Define the data
-    // fs.readFileSync returns an object (buffer)
-    const yml = fs.readFileSync('source/data/data.yml');
-
-    // Parse the data to JSON
-    // yamljs (YAML) requires a string and will not take an object
-    const data = YAML.parse(yml.toString());
-
-// =================== COMBINE TEMPLATE & DATA =============================
-
-    // Builds the html file using the template and data
-    const writeFile = fs.writeFileSync(newFilePath, compileTemplate(data));
+    const writeFile = fs.writeFileSync(newFilePath, template(data));
 });
