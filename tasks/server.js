751289ee4216de79a8d8d3e6c8653d15d3a7826c
Heather Taylor <heather@heysparkbox.com>
2017-06-02 17:05:08 -0400

    fix: reinstate auth
    - Remove comments from app.use(auth);
    - This line was commented out for ease of testing.

--- tasks/server.js
+++ tasks/server.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === 'production') {
   });
 
   app.use(noindex);
-  // app.use(auth); -- commented out for ease of testing
+  app.use(auth);
   // use express to look to send index.html to server
   app.use(express.static(path.join(__dirname, '../public')));
   // use express to look to send styles and other assets to server
