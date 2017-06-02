699a57af62c77f6a09bfc0858a5f9740b8c5ca06
Heather Taylor <heather@heysparkbox.com>
2017-06-02 16:52:54 -0400

    feat: update production server
    - Update production server to route to the new index.html file listed in the public directory
    - Update server.js file to comply with eslint recommendations
    - In testing of the production server, I discovered that the layouts partial was missing from the base partial which imports all styles into one common file. This was corrected.

--- tasks/server.js
+++ tasks/server.js
@@ -2,32 +2,28 @@
 
 const shell = require('shelljs');
 const express = require('express');
+
 const app = express();
 const path = require('path');
 const auth = require('marshmallows');
 const noindex = require('./lib/noindex');
 
-if (process.env.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === 'production') {
   // start production server
-  console.log("You have accessed the production server.");
-
   // process.env.PORT lets the port be set by Heroku
   const port = process.env.PORT || 5000;
 
-  app.listen(port, function() {
-    console.log("Our app is running on http://localhost:" + port);
+  app.listen(port, function () {
+    console.log('Our app is running on http://localhost:' + port);
   });
 
   app.use(noindex);
-  app.use(auth);
-
-  // make express look in the public directory for assets
+  // app.use(auth); -- commented out for ease of testing
+  // use express to look to send index.html to server
+  app.use(express.static(path.join(__dirname, '../public')));
+  // use express to look to send styles and other assets to server
   app.use(express.static(path.join(__dirname, '..')));
-
-
-}
-else {
+} else {
   // start development server to watch and update html and css files
-  console.log("You have accessed the development server.");
   shell.exec("browser-sync start --server --index 'public/index.html'  --files 'public/assets/css/*.css, public/*.html'");
 }
