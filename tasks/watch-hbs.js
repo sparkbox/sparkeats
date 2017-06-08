38e6356a856b4873ea035b2499787a6ff1dd7bc6
Betty Baker <betty@heysparkbox.com>
2017-06-08 11:51:05 -0400

    :bug: fix: Update hbs watch to watch partials in addition to pages

--- tasks/watch-hbs.js
+++ tasks/watch-hbs.js
@@ -3,4 +3,4 @@
 const shell = require('shelljs');
 
 // using onchange - throws an exec: internal error upon quitting
-shell.exec("onchange 'source/pages/*.hbs' -- npm run html");
+shell.exec("onchange 'source/**/*.hbs' -- npm run html");
