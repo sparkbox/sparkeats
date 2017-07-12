71e54e6af0a0b9c5a2d868ce4c485202a309a30d
Katy Bowman <katy@heysparkbox.com>
2017-07-12 14:23:26 -0400

    feat: adds function for combining yml files - adds getData to combine yml files - changes the template reference to getData() - fixes formatting issues

--- tasks/handlebars.js
+++ tasks/handlebars.js
@@ -6,20 +6,35 @@ const Handlebars = require('handlebars');
 const YAML = require('yamljs');
 const registerPartials = require('./register-partials');
 
+const readFile = (file) => fs.readFileSync(file, 'utf8');
+const ymlName = (filePath) => path.basename(filePath, '.yml');
+
 // This script compiles Handlebar templates, data, and partials
 // and writes a new HTML file in the public/ directory
 // New files are created with the same name as the Handlebar template.
 // Existing files are replaced.
 
+const getData = () => {
+  let dataFiles = 'data/*';
+  let dataFilePaths = path.join('source/', `${ dataFiles }.yml`);
+  let glob = globby.sync(dataFilePaths);
+  return glob.reduce((data, file) => {
+    let key = ymlName(file);
+    let val = YAML.parse(readFile(file));
+    data[key] = val;
+    return data;
+  }, {});
+};
+
 registerPartials('source/partials/*.hbs');
-const data = YAML.load('source/data/data.yml');
+const data = getData;
 const files = globby.sync('source/pages/*.hbs');
 
-files.forEach( function(file) {
-    const html = fs.readFileSync(file).toString();
-    const template = Handlebars.compile(html);
-    const fileName = path.basename(file, '.hbs');
-    const newFilePath = `public/${fileName}.html`;
+files.forEach(function (file) {
+  const html = fs.readFileSync(file).toString();
+  const template = Handlebars.compile(html);
+  const fileName = path.basename(file, '.hbs');
+  const newFilePath = `public/${fileName}.html`;
 
-    const writeFile = fs.writeFileSync(newFilePath, template(data));
+  const writeFile = fs.writeFileSync(newFilePath, template(getData()));
 });
