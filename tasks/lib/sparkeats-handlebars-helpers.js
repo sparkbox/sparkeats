56e9baf914849fdaefc3d70f96d8a92fe59fd693
Katy Bowman <katy@heysparkbox.com>
2017-07-13 15:53:49 -0400

    feat: removes dummy data and adds test data - removes all the dummy data and photos - adds real data and real photos

---
+++ tasks/lib/sparkeats-handlebars-helpers.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports.register = function (Handlebars) {
+  Handlebars.registerHelper('getPlaceImageLocation', (imageFileName) => {
+    if (!imageFileName) {
+      return 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
+    }
+    return `/assets/images/places/${imageFileName}`;
+  });
+
+  Handlebars.registerHelper('getReviewImageLocation', (imageFileName) => {
+    if (!imageFileName) {
+      return '';
+    }
+    return `/assets/images/reviews/${imageFileName}`;
+  });
+};
