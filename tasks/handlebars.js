417db0663aec69049d35ad5bb0dae605a5330aa0
Katy Bowman <katy@heysparkbox.com>
2017-07-21 15:42:54 -0400

    refactor: refactors how the data is processed and inserted into the handlebars templates - refactors how the data is assembled from the places and reviews yaml files - adds function for creating the reviews pages for each place

--- tasks/handlebars.js
+++ tasks/handlebars.js
@@ -1,43 +1,65 @@
 'use strict';
 
-const globby = require('globby');
 const fs = require('fs');
-const path = require('path');
 const Handlebars = require('handlebars');
 const YAML = require('yamljs');
 const registerPartials = require('./register-partials');
 const helpers = require('./lib/sparkeats-handlebars-helpers');
 
-const readFile = (file) => fs.readFileSync(file, 'utf8');
-const ymlName = (filePath) => path.basename(filePath, '.yml');
+const createTemplate = (file) => {
+  const html = fs.readFileSync(file).toString();
+  return Handlebars.compile(html);
+};
 
-// This script compiles Handlebar templates, data, and partials
-// and writes a new HTML file in the public/ directory
-// New files are created with the same name as the Handlebar template.
-// Existing files are replaced.
+const prepareReviewsPageData = (placeKey) => {
+  const reviews = YAML.load('source/data/reviews.yml');
+  const keys = Object.keys(reviews);
+  const placeReviews = {};
+  keys.forEach((key) => {
+    const review = reviews[key];
+    const placeId = review['place-id'];
+    if (placeId === placeKey) {
+      placeReviews[key] = review;
+    }
+  });
+  return placeReviews;
+};
 
-const getData = () => {
-  let dataFiles = 'data/*';
-  let dataFilePaths = path.join('source/', `${dataFiles}.yml`);
-  let glob = globby.sync(dataFilePaths);
-  return glob.reduce((data, file) => {
-    let key = ymlName(file);
-    let val = YAML.parse(readFile(file));
-    data[key] = val;
-    return data;
-  }, {});
+const prepareData = () => {
+  const places = YAML.load('source/data/places.yml');
+  const keys = Object.keys(places);
+  const data = {};
+  keys.forEach((key) => {
+    const place = places[key];
+    const reviews = prepareReviewsPageData(key);
+    data[key] = {
+      'place': place,
+      'reviews': reviews
+    };
+  });
+  return data;
+};
+
+const createIndexPage = () => {
+  const indexPageTemplate = createTemplate('source/pages/index.hbs');
+  const newFilePath = 'public/index.html';
+  const writeFile = fs.writeFileSync(newFilePath, indexPageTemplate(prepareData()));
+};
+
+const createReviewsPages = () => {
+  const reviewsPageTemplate = createTemplate('source/pages/review.hbs');
+  const places = YAML.load('source/data/places.yml');
+  const keys = Object.keys(places);
+  keys.forEach((key) => {
+    const newFilePath = `public/reviews/${key}.html`;
+    const data = prepareData();
+    const individualPlaceData = data[key];
+    const writeFile = fs.writeFileSync(newFilePath, reviewsPageTemplate(individualPlaceData));
+  });
 };
 
 registerPartials('source/partials/*.hbs');
 helpers.register(Handlebars);
 
-const files = globby.sync('source/pages/*.hbs');
-
-files.forEach(function (file) {
-  const html = fs.readFileSync(file).toString();
-  const template = Handlebars.compile(html);
-  const fileName = path.basename(file, '.hbs');
-  const newFilePath = `dist/${fileName}.html`;
-
-  const writeFile = fs.writeFileSync(newFilePath, template(getData()));
-});
+createIndexPage();
+createReviewsPages();
