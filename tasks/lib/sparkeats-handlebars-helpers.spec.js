6684496e32c2a912bda623855594cb50c28a820c
Katy Bowman <katy@heysparkbox.com>
2017-07-25 20:46:22 -0400

    test: clean up test code

--- tasks/lib/sparkeats-handlebars-helpers.spec.js
+++ tasks/lib/sparkeats-handlebars-helpers.spec.js
@@ -8,43 +8,42 @@ const expect = chai.expect;
 
 let html;
 let template;
+let result;
 
 sparkeatsHelpers.register(Handlebars);
 
 describe('getPlaceImageLocation', () => {
   before(() => {
-    html = '{{getPlaceImageLocation imgName}}';
+    html = '{{getPlaceImageLocation name}}';
     template = Handlebars.compile(html);
+    result = imgName => template({ name: imgName });
   });
   describe('if there is no image file name', () => {
     it('should return default background image', () => {
-      let imgName = { imgName: '' };
-      let result = template(imgName);
-      expect(result).to.equal('data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7');
+      expect(result('')).to.equal('data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7');
     });
   });
   describe('if there is an image file name', () => {
     it('should return the path to the image location in the places folder', () => {
-      let imgName = { imgName: 'test.jpg' };
-      let result = template(imgName);
-      expect(result).to.equal('/assets/images/places/test.jpg');
+      expect(result('test.jpg')).to.equal('/assets/images/places/test.jpg');
     });
   });
 });
 
 describe('getReviewImageLocation', () => {
   before(() => {
-    html = '{{getReviewImageLocation imgName}}';
+    html = '{{getReviewImageLocation name}}';
     template = Handlebars.compile(html);
+    result = imgName => template({ name: imgName });
   });
   describe('if there is no image file name', () => {
     it('should return an empty string', () => {
-      expect(template({ imgName: '' })).to.equal('');
+      expect(result('')).to.equal('');
     });
   });
   describe('if there is an image file name', () => {
     it('should return the path to the image location in the reviews folder', () => {
-      expect(template({ imgName: 'test.jpg' })).to.equal('/assets/images/reviews/test.jpg');
+      expect(result('test.jpg')).to.equal('/assets/images/reviews/test.jpg');
     });
   });
 });
@@ -53,20 +52,21 @@ describe('getReviewOrReviews', () => {
   before(() => {
     html = '{{getReviewOrReviews reviewCount}}';
     template = Handlebars.compile(html);
+    result = count => template({ reviewCount: count });
   });
   describe('if there are zero reviews', () => {
     it('should return "0 Reviews"', () => {
-      expect(template({ reviewCount: 0 })).to.equal('0 Reviews');
+      expect(result(0)).to.equal('0 Reviews');
     });
   });
   describe('if there is one review', () => {
     it('should return "1 Review"', () => {
-      expect(template({ reviewCount: 1 })).to.equal('1 Review');
+      expect(result(1)).to.equal('1 Review');
     });
   });
   describe('if there is more than one review', () => {
     it('should return "x Reviews"', () => {
-      expect(template({ reviewCount: 3 })).to.equal('3 Reviews');
+      expect(result(3)).to.equal('3 Reviews');
     });
   });
 });
