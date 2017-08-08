6ead8df1ee2b9488e836249217cb8a2694335b86
Katy Bowman <katy@heysparkbox.com>
2017-08-08 11:53:28 -0400

    refactor: refactor functions for easier testing

--- tasks/handlebars.js
+++ tasks/handlebars.js
@@ -11,17 +11,20 @@ const sortingData = require('./lib/sorting-data');
 registerPartials('source/partials/*.hbs');
 helpers.register(Handlebars);
 
+function loadYamlData(dataFilePath) {
+  return YAML.load(dataFilePath);
+}
+
 function createTemplate(file) {
   const html = fs.readFileSync(file).toString();
   return Handlebars.compile(html);
 }
 
-function prepareReviewsPageData(placeKey, reviewsDataPath) {
-  const reviews = YAML.load(reviewsDataPath);
-  const keys = Object.keys(reviews);
+function prepareReviewsPageData(placeKey, reviewsData) {
+  const keys = Object.keys(reviewsData);
   const placeReviews = {};
   keys.forEach((key) => {
-    const review = reviews[key];
+    const review = reviewsData[key];
     const placeId = review['place-id'];
     if (placeId === placeKey) {
       placeReviews[key] = review;
@@ -30,14 +33,13 @@ function prepareReviewsPageData(placeKey, reviewsDataPath) {
   return placeReviews;
 }
 
-function prepareAverageStars(reviewKey, reviewsDataPath) {
-  const reviews = YAML.load(reviewsDataPath);
-  const keys = Object.keys(reviews);
+function prepareAverageStars(reviewKey, reviewsData) {
+  const keys = Object.keys(reviewsData);
   const ratings = [];
   let averageStars;
   let ratingsSum;
   keys.forEach((key) => {
-    const review = reviews[key];
+    const review = reviewsData[key];
     const reviewId = review['place-id'];
     const numStars = review['number-of-stars'];
     if (reviewId === reviewKey) {
@@ -50,19 +52,23 @@ function prepareAverageStars(reviewKey, reviewsDataPath) {
     }
   });
   averageStars = ratingsSum / (ratings.length);
+
+  if (!averageStars) {
+    return '';
+  }
+
   averageStars = (Math.round(averageStars * 2) / 2).toFixed(1);
   return averageStars;
 }
 
-function prepareData(placesDataPath, reviewsDataPath) {
-  const places = YAML.load(placesDataPath);
-  const keys = Object.keys(places);
+function prepareData(placesData, reviewsData) {
+  const keys = Object.keys(placesData);
   const data = [];
-  const sortedKeys = sortingData.sortKeysByLocationAndPlace(places, keys);
+  const sortedKeys = sortingData.sortKeysByLocationAndPlace(placesData, keys);
   sortedKeys.forEach((key) => {
-    const place = places[key];
-    const reviews = prepareReviewsPageData(key, reviewsDataPath);
-    const averageStars = prepareAverageStars(key, reviewsDataPath);
+    const place = placesData[key];
+    const reviews = prepareReviewsPageData(key, reviewsData);
+    const averageStars = prepareAverageStars(key, reviewsData);
     const numberOfReviews = Object.keys(reviews).length;
     if (numberOfReviews > 0) {
       const placeData = {
@@ -78,15 +84,15 @@ function prepareData(placesDataPath, reviewsDataPath) {
   return data;
 }
 
-function createIndexPage(templatePath, newFilePath, placesDataPath, reviewsDataPath) {
+function createIndexPage(templatePath, newFilePath, placesData, reviewsData) {
   const indexPageTemplate = createTemplate(templatePath);
   const filePath = `${newFilePath}/index.html`;
-  const writeFile = fs.writeFileSync(filePath, indexPageTemplate(prepareData(placesDataPath, reviewsDataPath)));
+  const writeFile = fs.writeFileSync(filePath, indexPageTemplate(prepareData(placesData, reviewsData)));
 }
 
-function createReviewsPages(templatePath, newFilePath, placesDataPath, reviewsDataPath) {
+function createReviewsPages(templatePath, newFilePath, placesData, reviewsData) {
   const reviewsPageTemplate = createTemplate(templatePath);
-  const data = prepareData(placesDataPath, reviewsDataPath);
+  const data = prepareData(placesData, reviewsData);
   const keys = Object.keys(data);
   keys.forEach((key) => {
     const filePath = `${newFilePath}/${data[key]['place-id']}.html`;
@@ -100,10 +106,21 @@ function createReviewsPages(templatePath, newFilePath, placesDataPath, reviewsDa
   });
 }
 
-createIndexPage('source/pages/index.hbs', 'dist', 'public/data/places.yml', 'public/data/reviews.yml');
-createReviewsPages('source/pages/review.hbs', 'dist/reviews', 'public/data/places.yml', 'public/data/reviews.yml');
+function loadDataAndBuildPages(
+  indexTemplatePath,
+  reviewsTemplatePath,
+  placesDataPath,
+  reviewsDataPath) {
+  const placesData = loadYamlData(placesDataPath);
+  const reviewsData = loadYamlData(reviewsDataPath);
+  createIndexPage(indexTemplatePath, 'dist', placesData, reviewsData);
+  createReviewsPages(reviewsTemplatePath, 'dist/reviews', placesData, reviewsData);
+}
+
+loadDataAndBuildPages('source/pages/index.hbs', 'source/pages/review.hbs', 'public/data/places.yml', 'public/data/reviews.yml');
 
 module.exports = {
+  createTemplate,
   prepareReviewsPageData,
   prepareAverageStars,
   prepareData,
