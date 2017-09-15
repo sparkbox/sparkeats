16947bce92f06f194e8024a6af05eaedd441bfc0
Heather Taylor <heather@heysparkbox.com>
2017-09-15 09:47:41 -0400

    feat: update server tasks in preparation for launch
    - remove basic auth

--- tasks/server.js
+++ tasks/server.js
@@ -5,15 +5,11 @@ const express = require('express');
 
 const app = express();
 const path = require('path');
-const auth = require('marshmallows');
 const noindex = require('./lib/noindex');
 
 require('dotenv').config(path.join(__dirname, '/.env'));
-// require('dotenv').config();
 
 if (process.env.NODE_ENV === 'production') {
-  console.log('USER is ' + process.env.USER);
-  console.log('PASS is ' + process.env.PASS);
 
   // start production server (NODE_ENV=production npm start)
   // process.env.PORT lets the port be set by Heroku
@@ -24,7 +20,7 @@ if (process.env.NODE_ENV === 'production') {
   });
 
   app.use(noindex);
-  app.use(auth);
+
   // use express to look to send index.html to server
   app.use(express.static(path.join(__dirname, '../dist')));
   // use express to look to send styles and other assets to server
