0c3f816bba73b8d72515cb25726e563d8c3c52d1
Heather Taylor <heather@heysparkbox.com>
2017-09-18 12:04:30 -0400

    feat: add redirect from http to https to server tasks
    - use heroku-ssl-redirect node module to accomplish task
    
    feat: remove express-force-ssl from package.json and yarn.lock files

--- tasks/server.js
+++ tasks/server.js
@@ -2,6 +2,7 @@
 
 const shell = require('shelljs');
 const express = require('express');
+const sslRedirect = require('heroku-ssl-redirect');
 
 const app = express();
 const path = require('path');
@@ -15,6 +16,8 @@ if (process.env.NODE_ENV === 'production') {
   // process.env.PORT lets the port be set by Heroku
   const port = process.env.PORT || 5000;
 
+  app.use(sslRedirect());
+
   app.listen(port, function () {
     console.log('Our app is running on http://localhost:' + port);
   });
@@ -25,6 +28,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../dist')));
   // use express to look to send styles and other assets to server
   app.use(express.static(path.join(__dirname, '../public')));
+
 } else {
   // start development server (npm run dev) to watch and update html and css files
   shell.exec("browser-sync start --server --index 'dist/index.html' --serveStatic 'dist' 'public'");
