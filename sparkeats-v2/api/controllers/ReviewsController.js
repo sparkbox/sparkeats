bdd59c13d61a5e83b37bfa131a0f67b239870437
Yosevu Kilonzo <yosevu@heysparkbox.com>
2018-05-25 07:37:58 -0400

    feat (Reviews): Add create() action to ReviewsController * Add list() action to ReviewsController * Update routes

--- sparkeats-v2/api/controllers/ReviewsController.js
+++ sparkeats-v2/api/controllers/ReviewsController.js
@@ -6,7 +6,29 @@
  */
 
 module.exports = {
+  list: (req, res) => {
+    Reviews.find({}).exec((err, reviews) => {
+      if (err) {
+        res.send(500, { error: "Database Error" });
+      }
+      res.view("pages/homepage", { reviews });
+    });
+  },
   new: (req, res) => {
     res.view("pages/reviews/new");
+  },
+  create: (req, res) => {
+    const { visitorName, experience, visitedAt, imageName } = req.body;
+    const rating = parseInt(req.body.rating, 10);
+
+    Reviews.create({ visitorName, experience, rating, visitedAt, imageName }).exec(
+      err => {
+        if (err) {
+          res.send(500, { error: "Database Error" });
+        }
+
+        res.redirect("/");
+      }
+    );
   }
 };
