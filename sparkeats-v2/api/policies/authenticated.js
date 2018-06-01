639317c8becc0b85d4d0af9a21ffacff88b1a6d8
Travis <travis@devmaterial.com>
2018-06-01 12:06:04 -0400

    add authentication to  route

--- sparkeats-v2/api/policies/authenticated.js
+++ sparkeats-v2/api/policies/authenticated.js
@@ -1,29 +1,24 @@
-/**
- * api/policies/authenticated.js
- * 
- * This example shows how to use the HTTP Basic authentication strategy using the passport-http module.
- * Other strategies (Digest, OAuth, OAuth2, etc) can be similarly implemented.
- * 
-**/
+const express = require('express');
+const passport = require('passport');
 
-var express = require("express"),
-    app = express(),
-    passport = require("passport");
+const app = express();
 
 app.use(passport.initialize());
 
+module.exports = function authenticate(req, res, ok) {
+  passport.authenticate(
+    'basic',
+    {
+      session: false,
+    },
+    (err, user) => {
+      if (err || !user) {
+        res.set('WWW-Authenticate', 'Basic realm="Restricted"');
+        return res.send('You are not permitted to perform this action', 401);
+      }
 
-module.exports = function (req, res, ok) {
-    passport.authenticate("basic", {
-        session: false
-    }, function (err, user, info) {
-        if (err || !user) {
-            res.set("WWW-Authenticate", "Basic realm=\"Restricted\"");
-            return res.send("You are not permitted to perform this action", 401);
-        }
-
-        req.session.user = user;
-        return ok(null, user);
-        
-    })(req, res, ok);
+      req.session.user = user;
+      return ok(null, user);
+    }
+  )(req, res, ok);
 };
