0bb286cd44f44f07f966ed3f953778547ef43935
Yosevu Kilonzo <yosevu@heysparkbox.com>
2018-06-13 08:04:51 -0400

    feat: Deploy to Heroku * Add circle.yml * Temporarily add Sails files to ESLint * Update node scripts

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: node tasks/server.js
--- config/session.js
+++ config/session.js
@@ -10,30 +10,28 @@
  */
 
 module.exports.session = {
-
   /***************************************************************************
-  *                                                                          *
-  * Session secret is automatically generated when your new app is created   *
-  * Replace at your own risk in production-- you will invalidate the cookies *
-  * of your users, forcing them to log in again.                             *
-  *                                                                          *
-  ***************************************************************************/
+   *                                                                          *
+   * Session secret is automatically generated when your new app is created   *
+   * Replace at your own risk in production-- you will invalidate the cookies *
+   * of your users, forcing them to log in again.                             *
+   *                                                                          *
+   ***************************************************************************/
+
   secret: 'ee6e8b1cf6412f2a73120cead0c97ec2',
 
-
   /***************************************************************************
-  *                                                                          *
-  * Customize when built-in session support will be skipped.                 *
-  *                                                                          *
-  * (Useful for performance tuning; particularly to avoid wasting cycles on  *
-  * session management when responding to simple requests for static assets, *
-  * like images or stylesheets.)                                             *
-  *                                                                          *
-  * https://sailsjs.com/config/session                                       *
-  *                                                                          *
-  ***************************************************************************/
+   *                                                                          *
+   * Customize when built-in session support will be skipped.                 *
+   *                                                                          *
+   * (Useful for performance tuning; particularly to avoid wasting cycles on  *
+   * session management when responding to simple requests for static assets, *
+   * like images or stylesheets.)                                             *
+   *                                                                          *
+   * https://sailsjs.com/config/session                                       *
+   *                                                                          *
+   ***************************************************************************/
   // isSessionDisabled: function (req){
   //   return !!req.path.match(req._sails.LOOKS_LIKE_ASSET_RX);
   // },
-
 };
