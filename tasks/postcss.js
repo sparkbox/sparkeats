b6bcab8fed02cc5cb1aa1b581c4a53c6cfafcb0f
Yosevu Kilonzo <yosevu@heysparkbox.com>
2018-06-13 12:07:30 -0400

    chore: Remove Sparkeats 1.0 files and move Sparkeats 2.0 files to root directory

--- tasks/postcss.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const shell = require('shelljs');
-
-// use postcss to parse CSS and add vendor prefixes to CSS rules
-shell.exec('postcss --use autoprefixer -c ./tasks/postcss.config.json dist/assets/css/main.css -d dist/assets/css');
