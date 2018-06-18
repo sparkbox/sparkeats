0c2ee20f3b559370cec9d6daf493f7d91669c016
Travis <travis@devmaterial.com>
2018-06-18 09:43:32 -0400

    feat: Add place ratings and number of reviews

---
+++ sparkeats-v2/api/helpers/get-number-of-reviews.js
@@ -0,0 +1,23 @@
+module.exports = {
+  sync: true,
+  friendlyName: 'Get numbe rof reviews',
+
+  description: 'Returns the amount of reviews of a place.',
+
+  inputs: {
+    ratings: {
+      type: 'ref',
+      description: 'The ratings of a place.',
+    },
+    currentPlace: {
+      type: 'ref',
+      description: 'A single place.',
+    },
+  },
+
+  fn: ({ ratings, currentPlace }, exits) => {
+    const n = ratings.filter(rating => rating.placeId === currentPlace.id)
+      .length;
+    return exits.success(`${n} ${n === 1 ? 'Review' : 'Reviews'}`);
+  },
+};
