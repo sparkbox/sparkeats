84014db7aa94f0123bd9fe833d9ea777bfc9e894
Travis <travis@devmaterial.com>
2018-06-18 09:43:32 -0400

    feat: Add place ratings and number of reviews

--- sparkeats-v2/api/controllers/PlacesController.js
+++ sparkeats-v2/api/controllers/PlacesController.js
@@ -9,14 +9,31 @@ module.exports = {
   new: (req, res) => res.view('pages/places/new'),
   async places(req, res) {
     let places;
+    let reviews;
 
     try {
       places = await Place.find({}).intercept(err => err);
+      reviews = await Review.find({
+        select: ['numberOfStars', 'placeId'],
+      }).intercept(err => err);
     } catch (err) {
       return res.serverError(err);
     }
 
-    return res.view('pages/homepage', { places });
+    const dataForView = places.map(place => ({
+      id: place.id,
+      name: place.placeName,
+      address: `${place.city}, ${place.state}`,
+      phone: place.phone,
+      placeImage: place.placeImage,
+      placeImageAlt: place.placeImageAlt,
+      placeUrl: place.placeUrl,
+      placeWebsiteDisplay: place.placeWebsiteDisplay,
+      avgNumberOfStars: sails.helpers.getAvgNumberOfStars(reviews, place),
+      numberOfReviews: sails.helpers.getNumberOfReviews(reviews, place),
+    }));
+
+    return res.view('pages/homepage', { dataForView });
   },
   async create(req, res) {
     const {
