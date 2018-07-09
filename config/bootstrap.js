571470ebb82264d43ddca81906cf4bd120b63a90
Yosevu Kilonzo <yosevu@heysparkbox.com>
2018-07-09 07:46:34 -0400

    refactor: Refactor bootstrap.js and seed.js to work with test environment * Set up testing and lifecycle.js * Install mocha, chai, and supertest

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -1,46 +1,28 @@
 const { places, reviews } = require('../migrate');
-const seed = require('../seed');
+const { seedPlaces, seedReviews } = require('../seed');
+const dotenv = require('dotenv');
 
 async function bootstrap(done) {
-  require('dotenv').config();
+  dotenv.config();
 
   if (process.env.NODE_ENV === 'production') {
     return done();
   }
 
-  let placesCount;
-  let placeImageCount;
-  let reviewsCount;
-  let reviewImageCount;
+  return Promise
+    .all(seedPlaces(places))
+    .then(places => {
+      const seedPromises = places.map(place => {
+        const placeReviews = _.filter(reviews, review => {
+          return review['place-id'] === place.fd;
+        });
 
-  // try {
-  //   if ((await Place.count()) === 0) {
-  //     seed(places, reviews, done);
-  //   }
-  // } catch (err) {
-  //   return done(err);
-  // }
+        return Promise.all(seedReviews(place, placeReviews));
+      });
 
-  seed(places, reviews, done);
-
-  setTimeout(async () => {
-    try {
-      placesCount = await Place.count();
-      placeImageCount = await PlaceImage.count();
-      reviewsCount = await Review.count();
-      reviewImageCount = await ReviewImage.count();
-    } catch (err) {
-      return done(err);
-    }
-
-    console.log('Seeded:');
-    console.log(`- ${placesCount} places`);
-    console.log(`- ${placeImageCount} placeImages`);
-    console.log(`- ${reviewsCount} reviews`);
-    console.log(`- ${reviewImageCount} reviewImages`);
-  }, 1500);
-
-  return done();
+      return Promise.all(seedPromises);
+    })
+    .then(() => done())
+    .catch(done);
 }
-
 module.exports.bootstrap = bootstrap;
