a7107c518ad26df9913cbc52eafaf1fa5ebce1a5
Yosevu Kilonzo <yosevu@heysparkbox.com>
2018-07-09 07:52:03 -0400

    test: PlaceController and ReviewController actions   * Refactor places.ejs and reviews.ejs

--- api/controllers/review/create.js
+++ api/controllers/review/create.js
@@ -4,35 +4,39 @@ module.exports = async function create(req, res) {
   const placeId = req.param('id');
   const { reviewerName, reviewText, reviewImageAlt } = req.body;
   const numberOfStars = parseInt(req.body.numberOfStars, 10);
-  let reviewImage;
 
-  await req.file('reviewImage').upload(
+  req.file('reviewImage').upload(
     {
       adapter: SkipperMySQLAdapter,
       model: ReviewImage,
     },
     async (err, files) => {
       if (err) return res.serverError(err);
-      reviewImage = await ReviewImage.findOne({
-        fd: files[0].fd,
-      }).intercept(err => err);
 
       try {
+        let reviewImage = '';
+
+        if (files.length) {
+          reviewImage = await ReviewImage.findOne({
+            fd: files[0].fd,
+          }).intercept(err => err);
+
+          reviewImage = reviewImage.id;
+        }
+
         await Review.create({
           reviewerName,
           reviewText,
-          reviewImage: reviewImage.id,
+          reviewImage,
           reviewImageAlt,
           numberOfStars,
           placeId,
         })
-          .intercept('E_UNIQUE', err => err)
-          .intercept('UsageError', err => err);
+          .then(() => res.redirect(`/places/${placeId}`))
+          .catch(res.serverError);
       } catch (err) {
         return res.serverError(err);
       }
-
-      return res.redirect(`/places/${placeId}`);
     }
   );
 };
