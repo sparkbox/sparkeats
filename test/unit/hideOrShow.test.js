80895e9db8b2f1fa54c723b5435beb84ffa2f77a
Hunter Keca <hunter@heysparkbox.com>
2020-02-20 11:08:24 -0500

    test (hideOrShowCard): Add testing for hideOrShowCard

--- test/unit/hideOrShow.test.js
+++ test/unit/hideOrShow.test.js
@@ -6,12 +6,22 @@ const { expect } = require('chai');
 
 describe('Hide or show card', () => {
   it('returns show for All Places and hasHiddenClass = true', () => {
-    const result = hideOrShowCard("", "All Places", true);
+    const result = hideOrShowCard('', 'All Places', true);
     expect(result).to.equal('show');
   });
 
   it('returns show when city = location and hasHiddenClass = true', () => {
-    const result = hideOrShowCard("Dayton", "Dayton", true);
+    const result = hideOrShowCard('Dayton', 'Dayton', true);
     expect(result).to.equal('show');
+  });
+
+  it('returns hide when location != All Places and city != location and hasHiddenClass = false', () => {
+    const result = hideOrShowCard('', 'Dayton', false);
+    expect(result).to.equal('hide');
+  });
+
+  it('returns empty string when city != location and location != All Places and hasHiddenClass = true', () => {
+    const result = hideOrShowCard('', 'Ohio', true);
+    expect(result).to.equal('');
   });
 });
