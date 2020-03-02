c5c38e57bd9e15545f6cb7e938c740352c875f88
Luis Hernandez <luis@heysparkbox.com>
2020-03-02 13:48:09 -0500

    test: remove unnecessary mock data
    In showDropdownOptions.test.js remove unnecessary JSDOM elements.

--- test/unit/showDropdownOptions.test.js
+++ test/unit/showDropdownOptions.test.js
@@ -9,11 +9,6 @@ const { JSDOM } = jsdom;
 const dom = new JSDOM(`
 
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
 <body>
   <button id="location-dropdown__button-main" class="location-dropdown__button">
     <div class="location-dropdown__button-text">Pittsburgh</div>
@@ -25,97 +20,13 @@ const dom = new JSDOM(`
       </ul>
   </div
   <ul>
-  <li class="place-card__list-item">
-    <section class="place-card">
-      <header class="place-card__header">
-        <a href="/places/1">
-
-        </a>
-      </header>
-
-      <h3 class="place-card__name">
-        <a class="place-card__name-link" href="/places/1">Bourbon Street Grill</a>
-      </h3>
-      <p class="place-card__city">Dayton, OH</p>
-
-      <div class="place-card__star-rating" aria-label="5 stars">
-        <!-- average star rating -->
-        <img class="place-card__star" src="../design/sparkeats_star.svg" alt="review star" aria-hidden="true"><img
-          class="place-card__star" src="../design/sparkeats_star.svg" alt="review star" aria-hidden="true"><img
-          class="place-card__star" src="../design/sparkeats_star.svg" alt="review star" aria-hidden="true"><img
-          class="place-card__star" src="../design/sparkeats_star.svg" alt="review star" aria-hidden="true"><img
-          class="place-card__star" src="../design/sparkeats_star.svg" alt="review star" aria-hidden="true">
-      </div>
-
-      <address class="sparkeats-address">
-        <p>
-          1043 Brown Street
-        </p>
-
-        <p><a class="sparkeats-address__phone" href="tel:(937) 610-0083">(937) 610-0083</a></p>
-
-      </address>
-
-      <div class="sparkeats-address__url">
-        <a class="sparkeats-address__url-link" href="https://www.facebook.com/BourbonSyreetGrillandCafe1043/">Visit
-          Site</a>
-      </div>
-
-
-      <a class="place-card__footer-link" href="/places/1">
-        <!-- number of reviews -->
-
-        3 Reviews
-
-      </a>
-
-    </section>
-  </li>
-  <li class="place-card__list-item">
-    <section class="place-card">
-      <header class="place-card__header">
-        <a href="/places/2">
-
-        </a>
-      </header>
-
-      <h3 class="place-card__name">
-        <a class="place-card__name-link" href="/places/2">Jimmy Johns</a>
-      </h3>
+    <li class="place-card__list-item">
+        <p class="place-card__city">Dayton, OH</p>
+    </li>
+    <li class="place-card__list-item">
       <p class="place-card__city">Columbus, OH</p>
-
-      <div class="place-card__star-rating" aria-label="1 star">
-        <!-- average star rating -->
-        <img class="place-card__star" src="../design/sparkeats_star.svg" alt="review star" aria-hidden="true"><img
-          class="place-card__star" src="../design/sparkeats_star_empty.svg" alt="review star" aria-hidden="true"><img
-          class="place-card__star" src="../design/sparkeats_star_empty.svg" alt="review star" aria-hidden="true"><img
-          class="place-card__star" src="../design/sparkeats_star_empty.svg" alt="review star" aria-hidden="true"><img
-          class="place-card__star" src="../design/sparkeats_star_empty.svg" alt="review star" aria-hidden="true">
-      </div>
-
-      <address class="sparkeats-address">
-        <p>
-          1157 Brown Street
-        </p>
-
-        <p><a class="sparkeats-address__phone" href="tel:(937) 226-2600">(937) 226-2600</a></p>
-
-      </address>
-
-      <div class="sparkeats-address__url">
-        <a class="sparkeats-address__url-link" href="https://www.jimmyjohns.com">Visit Site</a>
-      </div>
-
-
-      <a class="place-card__footer-link" href="/places/2">
-        <!-- number of reviews -->
-
-        1 Review
-
-      </a>
-
-    </section>
-  </li>
+          tion>
+    </li>
   </ul>
 </body>
 </html>
