c611c9e6e4264351a560ef9191768da202343ac1
Luis Hernandez <luis@heysparkbox.com>
2020-03-03 14:16:04 -0500

    feat: add new route for redesign.
    Changes for the Sparkeats redesign will live at /refresh

---
+++ api/controllers/place/refresh.js
@@ -0,0 +1,61 @@
+const Promise = require('bluebird');
+const { findImageByID } = require('../../../lib/findImage');
+const getNumberOfReviews = require('../../../lib/getNumberOfReviews');
+const getNumberOfStars = require('../../../lib/getNumberOfStars');
+const { getAvgPlaceRating } = require('../../../lib/getAvgPlaceRating');
+const ratingToString = require('../../../lib/ratingToString');
+
+module.exports = async function getPlaces(req, res) {
+  Promise.props({
+    places: await Place.find({}),
+    reviews: await Review.find({
+      select: ['numberOfStars', 'placeId'],
+    }),
+  })
+    .then(({ places, reviews }) =>
+      Promise.all(
+        places.map(
+          async ({
+            id,
+            placeName,
+            city,
+            state,
+            address,
+            phone,
+            placeImage: placeImageID,
+            placeImageAlt,
+            placeURL,
+            placeWebsiteDisplay,
+          }) => {
+            const placeImage = await findImageByID(PlaceImage, placeImageID);
+            const numberOfStars = getAvgPlaceRating(id, reviews);
+
+            const starImagesString = getNumberOfStars(numberOfStars);
+
+            const rating = ratingToString(numberOfStars);
+
+            const numberOfReviews = getNumberOfReviews(reviews, id);
+
+            return {
+              id,
+              placeName,
+              city,
+              state,
+              address,
+              phone,
+              placeImage,
+              placeImageAlt,
+              placeURL,
+              placeWebsiteDisplay,
+              numberOfStars,
+              stars: starImagesString,
+              rating,
+              numberOfReviews,
+            };
+          }
+        )
+      )
+    )
+    .then(places => res.view('pages/homepage-refresh', { places }))
+    .catch(res.serverError);
+};
--- config/routes.js
+++ config/routes.js
@@ -2,8 +2,10 @@ module.exports.routes = {
   'GET /': 'PlaceController.places',
   'GET /places/new': 'PlaceController.add',
   'POST /places': 'PlaceController.create',
+  'GET /refresh': 'PlaceController.refresh',
 
   'GET /places/:id': 'ReviewController.reviews',
   'GET /places/:id/reviews/new': 'ReviewController.add',
   'POST /places/:id/reviews': 'ReviewController.create',
+
 };
