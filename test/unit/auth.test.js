0ed1393e695d858044c2e93f7bd5282512810167
Luis Hernandez <luis@heysparkbox.com>
2020-03-19 14:42:02 -0400

    fix: remove .only from unit test

--- test/unit/auth.test.js
+++ test/unit/auth.test.js
@@ -2,7 +2,7 @@ const { expect } = require('chai');
 const { validate } = require('../../api/services/auth-basic');
 
 describe('Authenticate', () => {
-  it.only('validates username and password', async () => {
+  it('validates username and password', async () => {
     const username = process.env.AUTH_USERNAME;
     const password = process.env.AUTH_PASSWORD;
     const result = await validate(username, password);
