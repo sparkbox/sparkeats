9201fe054e97645acd4fab772a7cf3e89d8ff68d
Ricardo Fearing <ricardo@heysparkbox.com>
2020-03-20 16:18:55 -0400

    Add audifred code

--- api/services/auth-basic.js
+++ api/services/auth-basic.js
@@ -2,21 +2,16 @@ const passport = require('passport');
 const { BasicStrategy } = require('passport-http');
 const compare = require('tsscmp');
 
-function validate(name, pass) {
-  let valid = true;
-
-  valid = compare(name, process.env.AUTH_USERNAME) && valid;
-  valid = compare(pass, process.env.AUTH_PASSWORD) && valid;
-
-  return valid;
-}
+const isValid = (name, pass) =>
+  compare(name, process.env.AUTH_USERNAME) &&
+  compare(pass, process.env.AUTH_PASSWORD);
 
 passport.use(
-  new BasicStrategy((username, password, next) => {
-    if (validate(username, password)) {
-      next(null, true);
-    } else {
-      next('not authorized');
-    }
-  })
+  new BasicStrategy((username, password, next) =>
+    isValid(username, password)
+      ? next(null, true)
+      : next(null, false, { message: 'not authorized' })
+  )
 );
+
+module.exports = { isValid };
--- test/unit/auth.test.js
+++ test/unit/auth.test.js
@@ -1,11 +1,27 @@
 const { expect } = require('chai');
-const { validate } = require('../../api/services/auth-basic');
+const { isValid } = require('../../api/services/auth-basic');
 
 describe('Authenticate', () => {
-  it('validates username and password', async () => {
-    const username = process.env.AUTH_USERNAME;
-    const password = process.env.AUTH_PASSWORD;
-    const result = await validate(username, password);
+  const authUserName = process.env.AUTH_USERNAME;
+  const authPassword = process.env.AUTH_PASSWORD;
+
+  before(() => {
+    process.env.AUTH_USERNAME = 'sample';
+    process.env.AUTH_PASSWORD = 'sample';
+  });
+
+  after(() => {
+    process.env.AUTH_USERNAME = authUserName;
+    process.env.AUTH_PASSWORD = authPassword;
+  });
+
+  it('validates username and password', () => {
+    const result = isValid('sample', 'sample');
     expect(result).to.equal(true);
+  });
+
+  it('returns false for incorrect credential', () => {
+    const result = isValid('wrong', 'credentials');
+    expect(result).to.equal(false);
   });
 });
