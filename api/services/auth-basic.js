44f280bf93aeafe98c4cbca4b6ef7bff8e595a7a
Luis Hernandez <luis@heysparkbox.com>
2020-03-20 13:53:41 -0400

    test: implement node process mock in test
    Instead of reading the .env file in the directory, implement a node
    process mock to test isValid Fn in auth-basic.js.

--- api/services/auth-basic.js
+++ api/services/auth-basic.js
@@ -2,16 +2,16 @@ const passport = require('passport');
 const { BasicStrategy } = require('passport-http');
 const compare = require('tsscmp');
 
-const validate = (name, pass) =>
+const isValid = (name, pass) =>
   compare(name, process.env.AUTH_USERNAME) &&
   compare(pass, process.env.AUTH_PASSWORD);
 
 passport.use(
   new BasicStrategy((username, password, next) =>
-    validate(username, password)
+    isValid(username, password)
       ? next(null, true)
       : next(null, false, { message: 'not authorized' })
   )
 );
 
-module.exports = { validate };
+module.exports = { isValid };
