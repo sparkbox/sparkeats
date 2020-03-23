54dbd8b97881a7ebe4db1756bd8d930de952f06b
Susmita Bhowmik <susmita@heysparkbox.com>
2020-03-23 09:51:08 -0400

    feat: add reviews-refresh route

--- config/routes.js
+++ config/routes.js
@@ -2,9 +2,10 @@ module.exports.routes = {
   'GET /': 'PlaceController.places',
   'GET /places/new': 'PlaceController.add',
   'POST /places': 'PlaceController.create',
-  'GET /refresh': 'PlaceController.refresh',
-
   'GET /places/:id': 'ReviewController.reviews',
   'GET /places/:id/reviews/new': 'ReviewController.add',
   'POST /places/:id/reviews': 'ReviewController.create',
+
+  'GET /refresh': 'PlaceController.refresh',
+  'GET /refresh/:id': 'ReviewController.reviews-refresh',
 };
