209c75d8e35f8b61cbddfa9df15f956d1a45d8c3
Yosevu Kilonzo <yosevu@heysparkbox.com>
2020-03-24 00:10:36 -0400

    refactor: simplify hideOrShowLocation in location-dropdown
    - fixes Code Climate linting errors
    - remove unnecessary tests
    - rename hideOrShowLocation tests

--- test/unit/hideOrShowLocation.test.js
+++ test/unit/hideOrShowLocation.test.js
@@ -2,23 +2,13 @@ const { hideOrShowLocation } = require('../../assets/js/local/locationDropdown')
 const { expect } = require('chai');
 
 describe('HideOrShowLocation', () => {
-  it('returns hide if elementId === location and hasHiddenClass === false', () => {
+  it('hides the city when the element id matches the location', () => {
     const result = hideOrShowLocation('Dayton', 'Dayton', false);
     expect(result).to.equal('hide');
   });
 
-  it('returns show if elementId !== location and hasHiddenClass === true', () => {
+  it('shows the city when the element id does not match the location', () => {
     const result = hideOrShowLocation('Dayton', 'Memphis', true);
     expect(result).to.equal('show');
-  });
-
-  it('returns empty string if elementId === location and hasHiddenClass === true', () => {
-    const result = hideOrShowLocation('Dayton', 'Dayton', true);
-    expect(result).to.equal('');
-  });
-
-  it('returns empty string if elementId !== location and hasHiddenClass === false', () => {
-    const result = hideOrShowLocation('Dayton', 'New York City', false);
-    expect(result).to.equal('');
   });
 });
