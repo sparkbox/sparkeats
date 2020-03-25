4bdf0a8dc35b3ca2d12ac8169e93caa9c225d8a5
Hunter Keca <hunter@heysparkbox.com>
2020-03-25 08:41:35 -0500

    chore (tasks): Add & register grunt-browser-sync

---
+++ tasks/config/browsersync.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+  grunt.config.set('browserSync', {
+    dev: {
+      bsFiles: {
+        src: [
+          '.tmp/**/*',
+          'views/**/*',
+        ]
+      },
+      options: {
+        // When your app also uses web sockets
+        // NOTE: requires 2.8.1 or above
+        proxy: {
+          target: 'http://localhost:1337',
+          ws: true,
+        },
+        watchTask: true,
+        reloadOnRestart: true,
+      },
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-browser-sync');
+};
