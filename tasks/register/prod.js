fb2b3ca8b30365535ac472b1da43ed5c57a0380f
Ricardo Fearing <ricardo@heysparkbox.com>
2020-04-02 13:19:15 -0400

    :bug: fix: update node version in package.json and update babelrc
    Updated babelrc file so that rewire babel plugin only runs in dev mode.

--- tasks/register/prod.js
+++ tasks/register/prod.js
@@ -11,7 +11,7 @@
  *   https://sailsjs.com/anatomy/tasks/register/prod.js
  *
  */
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   grunt.registerTask('prod', [
     'polyfill:prod', //« Remove this to skip transpilation in production (not recommended)
     'compileAssets',
