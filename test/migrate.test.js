b21bce0643db28b247eaf6872852775c3bb27a61
Susmita Bhowmik <susmita@heysparkbox.com>
2020-06-25 10:43:11 -0400

    test(migrate.test.js): update tests to represent the amount of items in the database

--- test/migrate.test.js
+++ test/migrate.test.js
@@ -2,12 +2,12 @@ const { expect } = require('chai');
 const { places, reviews } = require('../migrate');
 
 describe('Sparkeats 1.0 data', () => {
-  it('has 40 places', () => {
-    expect(Object.keys(places)).to.have.length(40);
+  it('has 12 places', () => {
+    expect(Object.keys(places)).to.have.length(12);
   });
 
-  it('has 51 reviews', () => {
-    expect(Object.keys(reviews)).to.have.length(51);
+  it('has 19 reviews', () => {
+    expect(Object.keys(reviews)).to.have.length(19);
   });
 
   xit('has 23 place images', () => {});
