bb01fce973f95731a4503a4d8879132882a161a7
Maaz Hamed <90350901+Maaz4324@users.noreply.github.com>
2022-10-08 03:24:08 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,29 @@
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+
 function App() {
   return (
-    <div className="">hello sparkeats</div>
+    <Router>
+      <div className="">hello sparkeats</div>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+          <li>
+            <Link to='/contact'>Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='/' element={<div>Home</div>}/>
+        <Route path='/about' element={<div>About</div>}/>
+        <Route path='/contact' element={<div>Contact page</div>}/>
+      </Routes>
+
+    </Router>
   )
 }
 
