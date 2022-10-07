2a6e662c9e2e879545c6e0ad8f7c9a0b8886796e
Yosevu Kilonzo <yosevuk@gmail.com>
2022-10-07 15:53:02 +0900

    refactor: remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-import { useState } from 'react'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="">Welcome to Sparkeats!</div>
+    <div className="">hello sparkeats</div>
   )
 }
 
