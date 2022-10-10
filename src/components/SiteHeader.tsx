9ac1aae11c6b52e77a696d1429aca93b50d71e42
Shyam Bhattarai <shyam.bhattarai@grepsr.com>
2022-10-10 22:53:41 +0545

    test: add and configure cypress tests
    test: update the tests to use accessible queries rather than class selectors
    
    test: update the tests to follow the end-to-end specifications
    
    test: update the package.json to run the tests
    
    test: add basic Home spec
    
    Resolves #387

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -2,7 +2,7 @@ export const SiteHeader = () => {
   return (
     <header className="site-header">
       <h1 className="site-header__title">Sparkeats by Sparkbox</h1>
-      <a className="site-header__logo" href="/" aria-label="Return to Sparkeats Homepage" />
+      <a className="site-header__logo" href="/" aria-label="Return to the Sparkeats Home page." />
     </header>
   );
 };
