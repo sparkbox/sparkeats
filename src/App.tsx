1875e02a84e05c69fe6f978b560036071ee6792a
Catherine Meade <cat@heysparkbox.com>
2022-10-17 15:11:39 -0400

    feat: SiteHeader has been rebuilt
    The SiteHeader component has been migrated from the old design.
    
    Updated some scss file names for a more consistent naming convention.
    
    Closes #401

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Outlet } from 'react-router-dom'
 import './App.scss'
+import { SiteHeader } from './components/SiteHeader';
 
 function App() {
   return (
     <div>
-      <header>Site Header</header>
+      <SiteHeader />
       <Outlet />
     </div>
   )
---
+++ src/components/SiteHeader.tsx
@@ -0,0 +1,9 @@
+export const SiteHeader = () => {
+  return (
+    <header className="site-header">
+      <h1 className="site-header__title">Sparkeats by Sparkbox</h1>
+      <a className="site-header__logo" href="/" aria-label="Return to Sparkeats Homepage" />
+    </header>
+  );
+};
+
