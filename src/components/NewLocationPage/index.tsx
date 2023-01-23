17cd157613f9a194008103e71658ce58c8689627
Yosevu Kilonzo <yosevuk@gmail.com>
2023-01-23 17:21:56 +0300

    refactor: uniform page layout

--- src/components/NewLocationPage/index.tsx
+++ src/components/NewLocationPage/index.tsx
@@ -1,8 +1,15 @@
+import { Link } from 'react-router-dom';
 import { NewLocationForm } from './NewLocationForm';
 
 export function NewLocationPage() {
   return (
-    <main className="new-location-page">
+    <main className="new-page">
+      <div className="review-nav">
+        <Link className="review-nav__link" to={'/'}>
+          <span className="review-nav__svg"></span>
+          Back to home
+        </Link>
+      </div>
       <NewLocationForm />
     </main>
   );
--- src/components/NewReviewPage/index.tsx
+++ src/components/NewReviewPage/index.tsx
@@ -1,9 +1,16 @@
+import { Link } from 'react-router-dom';
 import { NewReviewForm } from './NewReviewForm';
 import { ReviewHeader } from './ReviewHeader';
 
 export function NewReviewPage() {
   return (
-    <main className="review-page">
+    <main className="new-page">
+      <div className="review-nav">
+        <Link className="review-nav__link" to={'/'}>
+          <span className="review-nav__svg"></span>
+          Back to home
+        </Link>
+      </div>
       <ReviewHeader />
       <NewReviewForm />
     </main>
