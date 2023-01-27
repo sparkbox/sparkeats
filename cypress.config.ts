b276b053a7f22cb287b420f01c7b7e541c61809a
Yosevu Kilonzo <yosevuk@gmail.com>
2023-01-27 15:38:49 -0600

    chore: update Cypress
    - Add experimentalRunAllSpecs config option

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,5 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: 'http://127.0.0.1:5173/sparkeats/',
+    // Enables the "Run All Specs" UI feature, allowing the execution of multiple specs sequentially.
+    experimentalRunAllSpecs: true
   },
 });
